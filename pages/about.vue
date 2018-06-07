156d7b6aad3c55da91a6ab3d5bb1d7d7bd46b108
Mathieu TUDISCO <dev@mathieutu.ovh>
2018-06-07 10:00:29 +0200

    ✨ Add about page.

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <p>Serving from <strong v-text="from"/> for <strong v-text="userAgent"/></p>
+      <nuxt-link to="/">Home</nuxt-link>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    asyncData({req, isClient}) {
+        return {
+            userAgent: req ? req.headers['user-agent'] : navigator.userAgent,
+            from: isClient ? 'client' : 'server'
+        };
+    }
+};
+
+</script>
+
+<style scoped>
+</style>
