e544289a013f8419443c7108bec29270a1504209
Mathieu TUDISCO <dev@mathieutu.ovh>
2018-06-07 14:24:17 +0200

    ✨ Add meta in home page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,15 @@
 <template>
   <div>
     <h1>Hello world</h1>
-    <nuxt-link to="/about">About</nuxt-link>
   </div>
 
 
 </template>
 
 <script>
-
 export default {
-  components: {
+  head: {
+    title: "🖖",
   }
 }
 </script>
-
-<style>
-</style>
